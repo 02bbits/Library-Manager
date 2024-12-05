b6d2e99fe1af891174fbb92ab648bffa3de1cd20
bb02 <nnguyen.tuan0205@gmail.com>
2024-12-05 21:46:55 +0700

    - adjust login's fields - add password icon

--- src/view/LoginPanel.java
+++ src/view/LoginPanel.java
@@ -3,6 +3,8 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 import com.formdev.flatlaf.FlatClientProperties;
+import view.other.CustomPasswordField;
+import view.other.CustomTextField;
 
 public class LoginPanel extends JPanel {
 
@@ -17,7 +19,7 @@ public class LoginPanel extends JPanel {
         loginForm.setOpaque(false);
 
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 20, 10);
+        gbc.insets = new Insets(10, 45, 20, 10);
 
         // ============= Title label
         JLabel titleLabel1 = new JLabel("Login", SwingConstants.CENTER);
@@ -35,32 +37,34 @@ public class LoginPanel extends JPanel {
         loginForm.add(titleLabel1, gbc);
 
         gbc.gridy = 1;
-        gbc.insets = new Insets(0, 0, 40, 0);
+        gbc.insets = new Insets(0, 35, 40, 0);
         loginForm.add(titleLabel2, gbc);
         gbc.insets = new Insets(10, 10, 20, 10);
 
         // ============= Username label
-        JLabel usernameLabel = new JLabel(new ImageIcon("LibraryManagement/assets/icons/userIcon.png"));
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = 1;
-        gbc.anchor = GridBagConstraints.EAST;
-        loginForm.add(usernameLabel, gbc);
+//        JLabel usernameLabel = new JLabel(new ImageIcon("LibraryManagement/assets/icons/userIcon.png"));
+//        gbc.gridx = 0;
+//        gbc.gridy = 2;
+//        gbc.gridwidth = 1;
+//        gbc.anchor = GridBagConstraints.EAST;
+//        loginForm.add(usernameLabel, gbc);
 
         // ============= Password label
-        JLabel passwordLabel = new JLabel(new ImageIcon("LibraryManagement/assets/icons/passwordIcon.png"));
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.EAST;
-        loginForm.add(passwordLabel, gbc);
+//        JLabel passwordLabel = new JLabel(new ImageIcon("LibraryManagement/assets/icons/passwordIcon.png"));
+//        gbc.gridx = 0;
+//        gbc.gridy = 3;
+//        gbc.anchor = GridBagConstraints.EAST;
+//        loginForm.add(passwordLabel, gbc);
 
         // ============= Username field
-        JTextField usernameField = new JTextField(15);
-        usernameField.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "User Name");
-        usernameField.setBackground(Color.WHITE);
-        usernameField.putClientProperty(
-                FlatClientProperties.STYLE, "placeholderForeground:#a1a1a1"
-        );
+        CustomTextField usernameField = new CustomTextField();
+        usernameField.setPrefixIcon(new ImageIcon("LibraryManagement/assets/icons/userIcon.png"));
+        usernameField.setHint("Username");
+//        usernameField.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "User Name");
+//        usernameField.setBackground(Color.WHITE);
+//        usernameField.putClientProperty(
+//                FlatClientProperties.STYLE, "placeholderForeground:#a1a1a1"
+//        );
 
         gbc.gridx = 1;
         gbc.gridy = 2;
@@ -70,13 +74,14 @@ public class LoginPanel extends JPanel {
         loginForm.add(usernameField, gbc);
 
         // ============= Password field
-        JPasswordField passwordField = new JPasswordField(15);
-        passwordField.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Password");
+        CustomPasswordField passwordField = new CustomPasswordField();
+        passwordField.setPrefixIcon(new ImageIcon("LibraryManagement/assets/icons/passwordIcon.png"));
+        passwordField.setHint("Password");
         passwordField.putClientProperty(FlatClientProperties.STYLE, ""
                 + "showRevealButton:true;"
                 + "showCapsLock:true;"
                 + "placeholderForeground:#a1a1a1");
-        passwordField.setBackground(Color.WHITE);
+//        passwordField.setBackground(Color.WHITE);
 
         gbc.gridx = 1;
         gbc.gridy = 3;
@@ -89,12 +94,12 @@ public class LoginPanel extends JPanel {
         JButton btnLogin = new JButton("Login");
         btnLogin.putClientProperty(FlatClientProperties.STYLE,
                 "borderWidth:0; focusWidth:0");
-        btnLogin.setPreferredSize(new Dimension(210, 27));
+        btnLogin.setPreferredSize(new Dimension(50, 27));
         btnLogin.setBackground(new Color(227, 142, 73));
         btnLogin.setForeground(Color.BLACK);
         gbc.gridx = 0;
         gbc.gridy = 4;
-        gbc.gridwidth = 2;
+        gbc.gridwidth = 3;
         gbc.anchor = GridBagConstraints.CENTER;
         loginForm.add(btnLogin, gbc);
 
