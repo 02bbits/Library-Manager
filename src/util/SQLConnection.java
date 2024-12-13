01e91e2838977c614c52b8a4024ac766b0d657ef
bb02 <nnguyen.tuan0205@gmail.com>
2024-12-13 11:10:50 +0700

    - adjust sql connection url for new .db file

--- src/util/SQLConnection.java
+++ src/util/SQLConnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLConnection {
-    private static final String url = "jdbc:sqlite:LibraryManagement/resources/Library.db";
+    private static final String url = "jdbc:sqlite:LibraryManagement/resources/LibraryManagementSystem.db";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url);
